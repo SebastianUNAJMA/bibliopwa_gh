7454b7353fe71474ec3bef530f25c28cf4e17b69
SebastianUNAJMA <144154053+SebastianUNAJMA@users.noreply.github.com>
2025-11-12 20:28:51 +0000

    uno

---
+++ bibliotecapwa/src/app/app.config.ts
@@ -0,0 +1,30 @@
+//import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { __runInitializers } from 'tslib';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'
+import { environment } from '../environments/environment';
+import { provideAuth } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
+import { provideServiceWorker } from '@angular/service-worker';
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseconfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
+  ]
+};
---
+++ bibliotecapwa/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  // production: false es el valor por defecto en la convención antigua para este archivo
+  production: false,
+
+  // Configuración de Firebase para el desarrollo local
+  firebaseconfig: {
+    apiKey: "AIzaSyB0_dl54A-RDu0hGhNQ9QnFtSAE1mkROPo",
+    authDomain: "bibliotecapwa-909bc.firebaseapp.com",
+    projectId: "bibliotecapwa-909bc",
+    storageBucket: "bibliotecapwa-909bc.firebasestorage.app",
+    messagingSenderId: "1001860098147",
+    appId: "1:1001860098147:web:6156b0a69b235c87397056"
+  }
+};
